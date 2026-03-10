2163b5e6677f4097d75952e1ade8301a64b47600
Spivak-adam <Spivaka@oregonstate.edu>
2026-03-10 01:26:18 -0400

    port

--- backend/app/db.py
+++ backend/app/db.py
@@ -5,6 +5,7 @@ def connect_db():
     return mysql.connector.connect(
         host=os.getenv("MYSQL_HOST"),
         user=os.getenv("MYSQL_USER"),
+        port=int(os.getenv("MYSQL_PORT", "3306")),
         password=os.getenv("MYSQL_PASSWORD"),
         database=os.getenv("MYSQL_DATABASE"),
     )
